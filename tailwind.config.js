222fbd0862e3ff764188534d9e14a6f925004554
David Bobe <bobe.david@live.com>
2025-11-21 14:06:35 -0500

    Home Screen UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,ts,jsx,tsx}"],
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
     extend: {
       colors: {
         primary: "#030014",
         secondary: "#151312",
+        light: {
+          100: "#d6c7ff",
+          200: "#a8b5db",
+          300: "#9ca4ab",
+        },
+        dark: {
+          100: "#221F3D",
+          200: "#0f0d23",
+        },
         ratingBox: "#221F3D",
         searchBar: "#0F0D23",
         text: "#9CA4AB",
